4f97c8e2a22650177966a4089f1a51d79dce14ae
kermo-git <53812951+kermo-git@users.noreply.github.com>
2020-03-22 23:10:44 +0200

    Update AVL.cpp

--- AVL.cpp
+++ AVL.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
-#include <list>
-#include <sstream>
-#include <string>
-
 using namespace std;
 
-template <class K, class V>
-class AVL {
-    struct Node {
-        K key;
-        V value;
-        int height = 1;
-        Node* left = NULL;
-        Node* right = NULL;
 
-        Node(const K& k, const V& v) {
-            key = k; value = v;
+template <class K, class V>
+class AVLMap {
+    struct Node {
+        K key; V value;
+        Node* left = nullptr;
+        Node* right = nullptr;
+        int height = 1;
+
+
+        Node(const K& k) { key = k; }
+        Node(const Node& other) {
+            key = other.key;
+            value = other.value;
+
+            if (other.left)
+                left = new Node(*(other.left));
+            if (other.right)
+                right = new Node(*(other.right));
         }
-        Node(const K& k) {
-            key = k;
+        ~Node() {
+            if (left)
+                delete left;
+            if (right)
+                delete right;
         }
 
 
         void update_height() {
-            int l_height = (left == NULL)? 0 : left->height;
-            int r_height = (right == NULL)? 0 : right->height;
+            int l_height = (left)? left->height : 0;
+            int r_height = (right)? right->height : 0;
             height = (l_height > r_height)? 1 + l_height : 1 + r_height;
         }
         int balance_factor() {
-            int l_height = (left == NULL)? 0 : left->height;
-            int r_height = (right == NULL)? 0 : right->height;
+            int l_height = (left)? left->height : 0;
+            int r_height = (right)? right->height : 0;
             return l_height - r_height;
         }
 
@@ -74,91 +81,56 @@ class AVL {
             if (balancefactor == 2) {
                 if (left->balance_factor() == -1)
                     return left_right_rotation();
-                else
-                    return right_rotation();
+                return right_rotation();
             } else if (balancefactor == -2) {
                 if (right->balance_factor() == 1)
                     return right_left_rotation();
-                else
-                    return left_rotation();
+                return left_rotation();
             }
             update_height();
             return this;
         }
 
 
-        Node* get(const K& k) {
-            if (k < key)
-                return (left == NULL)? NULL : left->get(k);
-            if (k > key)
-                return (right == NULL)? NULL : right->get(k);
-            return this;
-        }
-
-
-        Node* smallest() {
-            Node* current = this;
-            while (current->left != NULL)
-                current = current->left;
-            return current;
-        }
-        Node* largest() {
-            Node* current = this;
-            while (current->right != NULL)
-                current = current->right;
-            return current;
-        }
-
-
         void print(ostream& os, Node* largest) {
-            if (left != NULL)
+            if (left)
                 left->print(os, largest);
             if (this == largest) {
                 os << key << "=" << value;
                 return;
             }
             os << key << "=" << value << ", ";
-            if (right != NULL)
+            if (right)
                 right->print(os, largest);
-            return;
         }
     };
 
 
-    Node* root = NULL;
+    Node* root = nullptr;
     int _size = 0;
 
 
-    void delete_rec(Node* node) {
-        if (node == NULL)
-            return;
-        delete_rec(node->left);
-        delete_rec(node->right);
-        delete node;
-    }
-
-
-    Node* clone(Node* other) {
-        if (other == NULL)
-            return NULL;
-        Node* result = new Node(other->key, other->value);
-        result->height = other->height;
-        result->left = clone(other->left);
-        result->right = clone(other->right);
-        return result;
+    Node* search(Node* node, const K& key) {
+        if (node) {
+            if (key < node->key)
+                return search(node->left, key);
+            if (key > node->key)
+                return search(node->right, key);
+        }
+        return node;
     }
 
 
     Node* temp;
-    Node* insert_key(Node* node, const K& key) {
-        if (node == NULL) {
+    Node* find_or_add_key(Node* node, const K& key) {
+        if (!node) {
             _size++;
             return temp = new Node(key);
         }
         if (key < node->key)
-            node->left = insert_key(node->left, key);
+            node->left = find_or_add_key(node->left, key);
         else if (key > node->key)
-            node->right = insert_key(node->right, key);
+            node->right = find_or_add_key(node->right, key);
         else {
             return temp = node;
         }
@@ -167,24 +139,27 @@ class AVL {
 
 
     Node* remove_key(Node* node, const K& key) {
-        if (node == NULL)
-            return NULL;
+        if (!node)
+            return nullptr;
         if (key < node->key)
             node->left = remove_key(node->left, key);
         else if (key > node->key)
             node->right = remove_key(node->right, key);
         else {
-            if (node->left == NULL) {
+            if (!(node->left)) {
                 _size--;
                 Node* tmp = node->right;
                 delete node; return tmp;
             }
-            if (node->right == NULL) {
+            else if (!(node->right)) {
                 _size--;
                 Node* tmp = node->left;
                 delete node; return tmp;
             } else {
-                Node* next = node->right->smallest();
+                Node* next = node->right;
+                while (next->left)
+                    next = next->left;
+
                 node->key = next->key;
                 node->value = next->value;
                 node->right = remove_key(node->right, node->key);
@@ -195,44 +170,53 @@ class AVL {
 
 
 public:
-    AVL() {}
-    AVL(const AVL<K, V>& other) {
-        root = clone(other.root);
+    AVLMap() {}
+    AVLMap(const AVLMap<K, V>& other) {
+        root = (other.root)? new Node(*(other.root)) : nullptr;
         _size = other._size;
     }
-    ~AVL() { clear(); }
+    ~AVLMap() {
+        if(root)
+            delete root;
+    }
 
 
     int size() { return _size; }
     bool empty() { return _size == 0; }
+    void clear() {
+        if (root)
+            delete root;
+        root = nullptr;
+        _size = 0;
+    }
+
+
     V& at(const K& key) {
-        if (root != NULL) {
-            Node *node = root->get(key);
-            if (node != NULL)
-                return node->value;
-        }
+        Node *node = search(root, key);
+        if (node)
+            return node->value;
         throw runtime_error("No such key.");
     }
     V& operator[](const K& key) {
-        root = insert_key(root, key);
+        root = find_or_add_key(root, key);
         return temp->value;
     }
     void erase(const K& key) {
         root = remove_key(root, key);
     }
     bool contains_key(const K& key) {
-        return root->get(key) != NULL;
-    }
-    void clear() {
-        delete_rec(root);
-        root = NULL; _size = 0;
+        return (bool) search(root, key);
     }
 
 
-    friend ostream& operator<<(ostream& os, AVL& map) {
+    friend ostream& operator<<(ostream& os, AVLMap& map) {
         os << "{";
-        if (map.root != NULL)
-            map.root->print(os, map.root->largest());
+        if (map.root) {
+            AVLMap::Node *largest = map.root;
+            while (largest->right)
+                largest = largest->right;
+            map.root->print(os, largest);
+        }
         os << "}";
         return os;
     }
