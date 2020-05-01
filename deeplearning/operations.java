74bdcfd8bcd07b5c75f8a88bf8398f325a369769
kermo-git <53812951+kermo-git@users.noreply.github.com>
2020-05-01 17:46:38 +0300

    Update operations.java

--- deeplearning/operations.java
+++ deeplearning/operations.java
@@ -2,9 +2,9 @@ package deeplearning;
 import java.util.Random;
 
 
-abstract class Operation {
-    protected Operation left = null;
-    protected Operation right = null;
+class Operation {
+    protected Operation left;
+    protected Operation right;
 
 
     protected int rows, cols;
@@ -12,45 +12,15 @@ abstract class Operation {
     protected double[] gradient;
 
 
-    protected Operation(int rows, int cols) {
-        this.rows = rows;
-        this.cols = cols;
-
-        output = new double[rows*cols];
-        gradient = new double[rows*cols];
+    public Operation(int rows, int cols) {
+        init(rows, cols);
     }
-    protected Operation(Operation input) {
-        this(input.rows, input.cols);
-        left = input;
-    }
-    protected Operation(Operation left, Operation right) {
-        this(left.rows, left.cols);
-
-        if (left.cols != right.cols || left.rows != right.rows)
-            throw new IllegalArgumentException();
-
-        this.left = left;
-        this.right = right;
-    }
+    protected Operation() {}
 
 
-    public abstract void forward();
-    public abstract void backward();
 
-
-    protected void setGradient(int index, double grad) {
-        gradient[index] = grad;
-    }
-    public void setOutput(double[] output) {
-        this.output = output;
-    }
-}
-
-
-class Matrix extends Operation {
-    public Matrix(int rows, int cols) {
-        super(rows, cols);
-    }
+    public void forward() {}
+    public void backward() {}
 
 
     private void init(double std_dev) {
@@ -62,30 +32,86 @@ class Matrix extends Operation {
     }
     // Default initialisation
     public void xavier_init() { init(Math.sqrt(1.0/cols)); }
-    // Use with ReLU
+    // Use with RELU
     public void he_init() { init(Math.sqrt(2.0/cols)); }
 
 
-    @Override
-    public void forward() {}
-    @Override
-    public void backward() {}
-    @Override
-    protected void setGradient(int index, double grad) {
-        gradient[index] += grad;
-    }
-    public void update(double learning_rate, double batch_size) {
-        for (int i = 0; i < output.length; i++) {
-            output[i] -= (gradient[i]/batch_size)*learning_rate;
-            gradient[i] = 0;
+    public void init() {
+        if (left != null) {
+            if (right != null && (left.cols != right.cols || left.rows != right.rows))
+                throw new IllegalArgumentException();
+
+            this.rows = left.rows;
+            this.cols = left.cols;
         }
+        output = new double[rows*cols];
+        gradient = new double[rows*cols];
+    }
+
+
+    public void init(int rows, int cols) {
+        this.rows = rows;
+        this.cols = cols;
+
+        output = new double[rows*cols];
+        gradient = new double[rows*cols];
+    }
+
+
+    public void update(double learning_rate) {
+        for (int i = 0; i < output.length; i++) {
+            output[i] -= gradient[i]*learning_rate;
+        }
+    }
+
+    
+    public void setInput(Operation input) {
+        left = input; right = null;
+    }
+    public void setInput(Operation left, Operation right) {
+        this.left = left; this.right = right;
+    }
+    protected void setGradient(int index, double grad) {
+        gradient[index] = grad;
+    }
+
+
+    public String toString() {
+        int max_len = 0;
+        for (int i = 0; i < output.length; i++) {
+            int len = Double.toString(output[i]).length();
+            if (len > max_len)
+                max_len = len;
+        }
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+
+        for (int row = 0; row < rows; row++) {
+            sb.append("[");
+            for (int col = 0; col < cols; col++) {
+                String num = Double.toString(output[index]);
+                int spaces = max_len - num.length();
+
+                for (int i = 0; i < spaces; i++) {
+                    sb.append(" ");
+                }
+                if (col < cols - 1)
+                    sb.append(output[index] + ", ");
+                else
+                    sb.append(output[index]);
+                index++;
+            }
+            sb.append("]\n");
+        }
+        return sb.toString();
     }
 }
 
 
 class Addition extends Operation {
     public Addition(Operation left, Operation right) {
-        super(left, right);
+        this.left = left;
+        this.right = right;
     }
 
 
@@ -98,6 +124,8 @@ class Addition extends Operation {
             output[i] = left_out[i] + right_out[i];
         }
     }
+
+
     @Override
     public void backward() {
         for (int i = 0; i < output.length; i++) {
@@ -110,7 +138,8 @@ class Addition extends Operation {
 
 class Subtraction extends Operation {
     public Subtraction(Operation left, Operation right) {
-        super(left, right);
+        this.left = left;
+        this.right = right;
     }
 
 
@@ -123,6 +152,8 @@ class Subtraction extends Operation {
             output[i] = left_out[i] - right_out[i];
         }
     }
+
+
     @Override
     public void backward() {
         for (int i = 0; i < output.length; i++) {
@@ -135,7 +166,8 @@ class Subtraction extends Operation {
 
 class Multiplication extends Operation {
     public Multiplication(Operation left, Operation right) {
-        super(left, right);
+        this.left = left;
+        this.right = right;
     }
 
 
@@ -148,6 +180,8 @@ class Multiplication extends Operation {
             output[i] = left_out[i]*right_out[i];
         }
     }
+
+
     @Override
     public void backward() {
         double[] left_out = left.output;
@@ -163,7 +197,8 @@ class Multiplication extends Operation {
 
 class Division extends Operation {
     public Division(Operation left, Operation right) {
-        super(left, right);
+        this.left = left;
+        this.right = right;
     }
 
 
@@ -176,6 +211,8 @@ class Division extends Operation {
             output[i] = left_out[i]/right_out[i];
         }
     }
+
+
     @Override
     public void backward() {
         double[] left_out = left.output;
@@ -186,7 +223,7 @@ class Division extends Operation {
             grad = gradient[i];
             l = left_out[i];
             r = right_out[i];
-            
+
             left.setGradient(i, grad/r);
             right.setGradient(i, -grad*l/(r*r));
         }
@@ -199,7 +236,7 @@ class RELU extends Operation {
 
 
     public RELU(Operation input, double leak) {
-        super(input);
+        this.left = input;
         this.leak = leak;
     }
     public RELU(Operation input) {
@@ -215,6 +252,8 @@ class RELU extends Operation {
             output[i] = (input[i] > 0)? input[i] : leak*input[i];
         }
     }
+
+
     @Override
     public void backward() {
         for (int i = 0; i < output.length; i++) {
@@ -231,7 +270,7 @@ class SELU extends Operation {
 
 
     public SELU(Operation input) {
-        super(input);
+        this.left = input;
     }
 
 
@@ -246,6 +285,8 @@ class SELU extends Operation {
                 output[i] = LAMBDA*ALPHA*(Math.exp(input[i]) - 1);
         }
     }
+
+
     @Override
     public void backward() {
         double[] input = left.output;
@@ -265,7 +306,7 @@ class SELU extends Operation {
 
 class Sigmoid extends Operation {
     public Sigmoid(Operation input) {
-        super(input);
+        this.left = input;
     }
 
 
@@ -277,6 +318,8 @@ class Sigmoid extends Operation {
             output[i] = 1/(1 + Math.exp(-input[i]));
         }
     }
+
+
     @Override
     public void backward() {
         double[] grad = left.gradient;
@@ -290,7 +333,7 @@ class Sigmoid extends Operation {
 
 class Tanh extends Operation {
     public Tanh(Operation input) {
-        super(input);
+        this.left = input;
     }
 
 
@@ -299,12 +342,12 @@ class Tanh extends Operation {
         double[] input = left.output;
 
         for (int i = 0; i < output.length; i++) {
-            double pos_exp = Math.exp(input[i]);
-            double neg_exp = Math.exp(-input[i]);
-
-            output[i] = (pos_exp - neg_exp)/(pos_exp + neg_exp);
+            double exp = Math.exp(2*input[i]);
+            output[i] = (exp - 1)/(exp + 1);
         }
     }
+
+
     @Override
     public void backward() {
         for (int i = 0; i < output.length; i++) {
@@ -314,15 +357,77 @@ class Tanh extends Operation {
 }
 
 
+class Dropout extends Operation {
+    private final double p;
+    double[] mask;
+    boolean train_mode;
+    Random random = new Random();
+
+
+    public Dropout(Operation input, double p) {
+        this.left = input;
+        if (p < 0 || p > 1)
+            throw new IllegalArgumentException();
+        this.p = p;
+    }
+    public void setTrainMode(boolean mode) {
+        train_mode = mode;
+    }
+
+
+    public void init() {
+        super.init();
+        mask = new double[left.rows*left.cols];
+    }
+
+
+    @Override
+    public void forward() {
+        double[] input = left.output;
+
+        if (train_mode) {
+            for (int i = 0; i < output.length; i++) {
+                mask[i] = (random.nextDouble() <= p)? 1/p : 0;
+                output[i] = input[i]*mask[i];
+            }
+        }
+        else {
+            for (int i = 0; i < output.length; i++) {
+                output[i] = input[i];
+            }
+        }
+    }
+
+
+    @Override
+    public void backward() {
+        double[] input = left.output;
+
+        double grad;
+        for (int i = 0; i < output.length; i++) {
+            left.setGradient(i, gradient[i]*mask[i]);
+        }
+    }
+}
+
+
 class DotProduct extends Operation {
     public DotProduct(Operation left, Operation right) {
-        super(left.rows, right.cols);
+        this.left = left;
+        this.right = right;
+    }
 
+
+    @Override
+    public void init() {
         if (left.cols != right.rows)
             throw new IllegalArgumentException();
 
-        this.left = left;
-        this.right = right;
+        this.rows = left.rows;
+        this.cols = right.cols;
+
+        output = new double[rows*cols];
+        gradient = new double[rows*cols];
     }
 
 
@@ -330,11 +435,11 @@ class DotProduct extends Operation {
     public void forward() {
         double[] left_out = left.output;
         double[] right_out = right.output;
+
         int index = 0;
 
         for (int row_start = 0; row_start < left_out.length; row_start += left.cols) {
             for (int col_start = 0; col_start < cols; col_start++) {
-
                 int i = row_start;
                 int j = col_start;
 
@@ -358,23 +463,20 @@ class DotProduct extends Operation {
 
                 int right_i = col*cols;
                 int this_i = row*cols;
-
+                
                 while (this_i < (row + 1)*cols) {
                     grad += gradient[this_i]*right.output[right_i];
                     this_i++;
                     right_i++;
                 }
-
                 left.setGradient(grad_i, grad); grad_i++;
             }
         }
-
         grad_i = 0;
-
+        
         for (int row = 0; row < right.rows; row++) {
             for (int col = 0; col < cols; col++) {
                 double grad = 0;
-
                 int left_i = row;
                 int this_i = col;
 
@@ -383,7 +485,6 @@ class DotProduct extends Operation {
                     this_i += cols;
                     left_i += left.cols;
                 }
-
                 right.setGradient(grad_i, grad); grad_i++;
             }
         }
@@ -396,33 +497,53 @@ class SoftmaxCrossEntropy extends Operation {
 
 
     public SoftmaxCrossEntropy(Operation input, Operation label) {
-        super(input, label);
+        this.left = input;
+        this.right = label;
     }
 
 
     @Override
     public void forward() {
         double[] input = left.output;
-        double max_input = input[0];
-
-        for (int i = 0; i < output.length; i++) {
-            max_input = (input[i] > max_input)? input[i] : max_input;
-        }
-
-        double exp_sum = 0;
-
-        for (int i = 0; i < output.length; i++) {
-            exp_sum += (output[i] = Math.exp(input[i] - max_input));
-        }
-
         double[] label = right.output;
-        error = 0;
 
-        for (int i = 0; i < output.length; i++) {
-            output[i] /= exp_sum;
-            if (label[i] != 0)
-                error -= Math.log(output[i]);
+        double[] tmp = new double[cols];
+        double[] exp_sums = new double[cols];
+
+        int index = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                if (input[index] > tmp[col])
+                    tmp[col] = input[index];
+                index++;
+            }
         }
+
+        index = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                output[index] = Math.exp(output[index] - tmp[col]);
+                exp_sums[col] += output[index];
+                index++;
+            }
+        }
+
+        index = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < cols; col++) {
+                output[index] /= exp_sums[col];
+
+                if (label[index] != 0)
+                    tmp[col] = -Math.log(output[index]);
+
+                index++;
+            }
+        }
+
+        for (double col_error : tmp) {
+            error += col_error;
+        }
+        error /= cols;
     }
 
 
@@ -431,7 +552,106 @@ class SoftmaxCrossEntropy extends Operation {
         double[] label = right.output;
 
         for (int i = 0; i < output.length; i++) {
-            left.setGradient(i, output[i] - label[i]);
+            left.setGradient(i, (output[i] - label[i])/cols);
         }
+    }
+}
+
+
+class MeanSquareError extends Operation {
+    private double error = 0;
+
+
+    public MeanSquareError(Operation input, Operation label) {
+        this.left = input;
+        this.right = label;
+    }
+
+
+    @Override
+    public void init() {}
+
+
+    @Override
+    public void forward() {
+        double[] prediction = left.output;
+        double[] label = right.output;
+
+        double difference;
+        for (int i = 0; i < prediction.length; i++) {
+            difference = prediction[i] - label[i];
+            error += difference*difference;
+        }
+        error /= prediction.length;
+    }
+
+
+    @Override
+    public void backward() {
+        double[] prediction = left.output;
+        double[] label = right.output;
+
+        for (int i = 0; i < prediction.length; i++) {
+            left.setGradient(i, 2*(prediction[i] - label[i])/prediction.length);
+        }
+    }
+}
+
+
+class Dense extends Operation {
+    int n;
+
+    Operation W = new Operation();
+    Operation b = new Operation();
+
+    Operation dot = new DotProduct(W, null);
+    Operation add = new Addition(dot, b);
+    Operation activation;
+
+    public Dense(int n, String activation) {
+        this.n = n;
+
+        switch (activation) {
+            case "sigmoid":
+                this.activation = new Sigmoid(add); break;
+            case "tanh":
+                this.activation = new Tanh(add); break;
+            case "relu":
+                this.activation = new RELU(add); break;
+            case "selu":
+                this.activation = new SELU(add); break;
+        }
+    }
+
+
+    @Override
+    public void init() {
+        W.init(n, left.rows);
+        b.init(n, 1);
+
+        dot.setInput(W, left);
+        dot.init();
+        add.init();
+        activation.init();
+
+        rows = activation.rows;
+        cols = activation.cols;
+        output = activation.output;
+    }
+
+
+    @Override
+    public void forward() {
+        dot.forward();
+        add.forward();
+        activation.forward();
+    }
+
+
+    @Override
+    public void backward() {
+        activation.backward();
+        add.backward();
+        dot.backward();
     }
 }
